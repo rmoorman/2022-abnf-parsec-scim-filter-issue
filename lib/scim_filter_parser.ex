49eb4c921f4e95148a2b5975c1d62f259dc4455b
Rico Moorman <rico.moorman@gmail.com>
2022-09-12 21:34:52 +0200

    Add SCIM abnf, example parsers and tests

---
+++ lib/scim_filter_parser.ex
@@ -0,0 +1,21 @@
+defmodule ScimFilterParser do
+  use AbnfParsec, [
+    abnf_file: "priv/scim.abnf",
+    #transform: %{
+    #  "ATTRNAME" => {:reduce, {List, :to_string, []}},
+    #  "string" => {:reduce, {List, :to_string, []}},
+    #},
+    #ignore: [
+    #  "quotation-mark",
+    #],
+    #unbox: [
+    #  "nameChar",
+    #  "json-char",
+    #  "unescaped",
+    #  "string",
+    #  "ATTRNAME",
+    #  "subAttr",
+    #],
+    parse: :filter,
+  ]
+end
